2cd3a85426e3980f34a3c6cbd12898f005fcac32
Michael Burke <mike.d.burke@gmail.com>
2016-06-25 23:38:29 -0400

    Add tests.

--- cms.rb
+++ cms.rb
@@ -1,10 +1,14 @@
-require "sinatra"
-require "sinatra/reloader" if development?
-require "sinatra/content_for"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/content_for'
+require 'tilt/erubis'
+require 'pry'
+
+root = File.expand_path('..', __FILE__)
 
 configure do
-
+  enable :sessions
+  set :session_secret, 'secret'
 end
 
 helpers do
@@ -15,6 +19,20 @@ before do
 
 end
 
-get "/" do
-  "Getting Started."
+get '/' do
+  @files = Dir.glob(root + '/data/*').map do |path|
+    File.basename(path)
+  end
+  erb :index
+end
+
+get '/:filename' do
+  file_path = root + '/data/' + params[:filename]
+  headers["Content-Type"] = "text/plain"
+  if File.exist?(file_path)
+    File.read(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect '/'
+  end
 end
---
+++ test/cms_test.rb
@@ -0,0 +1,40 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+
+  def test_filename
+    get "/history.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "1995 - Ruby 0.95 released."
+  end
+
+  def test_bad_filename
+    get "/notafile.ext"
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist"
+  end
+end
